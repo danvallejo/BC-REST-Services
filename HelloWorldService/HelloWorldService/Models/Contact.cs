e454e7c1140fd804ded8cf698e56f955e36f5dff
dan <danv@cstructor.com>
2016-01-11 20:02:17 -0800

    add jsonproperty to fields and make everything snake_case

--- HelloWorldService/HelloWorldService/Models/Contact.cs
+++ HelloWorldService/HelloWorldService/Models/Contact.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace HelloWorldService.Models
 {
     public class Contact
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("date_added", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public DateTime DateAdded { get; set; }
+
+        [JsonProperty("phones", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public Phone[] Phones { get; set; }
     }
 
     public class Phone
     {
+        [JsonProperty("number", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Number { get; set; }
+
+        [JsonProperty("phone_type")]
+        [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
         public PhoneType PhoneType { get; set; }
     }
 
     public enum PhoneType
     {
+        Unknown,
         Home,
         Mobile,
     }
